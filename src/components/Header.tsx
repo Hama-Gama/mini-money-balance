699d07d8dd267d16214826c0677195054656c767
Khamit <admiral3nelson7@gmail.com>
2025-12-23 10:47:07 +0500

    style: week number circle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,22 @@ export const Header = () => {
 
 	return (
 		<div className='flex items-center justify-between border-b pb-2'>
-			<div className='text-lg font-semibold'>{weekNumber}</div>
+			{/* Номер недели в круге */}
+			<div
+				className='
+          w-6
+          h-6
+          flex
+          items-center
+          justify-center
+          rounded-full
+          bg-black
+          text-white
+          font-semibold
+        '
+			>
+				{weekNumber}
+			</div>
 
 			<div className='text-base text-muted-foreground'>
 				{date.toLocaleDateString('ru-RU', {
