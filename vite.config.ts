35554ce7f6404f5dcb0ab3a55a1e94ed1cc15b5c
Khamit <admiral3nelson7@gmail.com>
2026-02-06 13:18:08 +0500

    feature: offline working, vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,41 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.svg'],
+			manifest: {
+				name: 'Finance Tracker',
+				short_name: 'Finance',
+				start_url: '/',
+				display: 'standalone',
+				background_color: '#ffffff',
+				theme_color: '#000000',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
