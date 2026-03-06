8b33c6114a8ff6c1136a11e94e48b70926cf09ce
Khamit <admiral3nelson7@gmail.com>
2026-03-07 00:43:03 +0500

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export const Header = ({}: Props) => {
 	const weekNumber = Math.ceil(dayOfMonth / 7)
 
 	return (
-		<div className='fixed top-0 left-0 right-0 z-50 bg-white border-b'>
-			<div className='max-w-md mx-auto px-3 pb-6 pt-2 flex items-center justify-between'>
+		<div className='fixed top-0 left-0 right-0 z-50 bg-zinc-100 border-b'>
+			<div className='max-w-md mx-auto px-3 py-4 flex items-center justify-between'>
 				<div className='w-7 h-7 flex items-center justify-center bg-black rounded-full text-white font-semibold'>
 					{weekNumber}
 				</div>
